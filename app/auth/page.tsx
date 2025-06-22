bf8d316af0be5a346f299c1e2e7073bdda13ba33
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-06-22 18:42:13 +0530

    fixed comp

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,4 @@
-import LoginButton from '@/components/LoginButton'
+import LoginButton from '../../components/LoginButton'
 import { requireGuest } from '@/lib/auth-utils'
 import React from 'react'
 import { 
