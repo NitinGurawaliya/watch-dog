43a6a2573af937400c161623416e03f61f8d022a
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-06-22 22:59:18 +0530

    kjn

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -4,7 +4,7 @@ import {
   ArrowRightEndOnRectangleIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import LoginButton from '../../components/LoginButton';
+import LoginButton from '@/components/LoginButton';
 
 const Auth = async () => {
   // This will redirect authenticated users to /dashboard
